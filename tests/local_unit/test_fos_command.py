1c95bc77435a6b6945dd5a100a45b6829c730cd4
hs0210 <hus.fnst@cn.fujitsu.com>
2020-12-17 16:38:58 +0800

    update fos_command local_unit_test
    Signed-off-by: hs0210 <hus.fnst@cn.fujitsu.com>

--- tests/local_unit/test_fos_command.py
+++ tests/local_unit/test_fos_command.py
@@ -12,34 +12,40 @@ PlaybookPath = 'tests/local_unit/playbooks/fos_command/'
 
 class Test(unittest.TestCase):
     def test_fos_command_simple(self):
-        command = Cli + Inventory + PlaybookPath + 'show_version.yaml -vvv | grep "Current Runtime Version"'
+        command = Cli + Inventory + PlaybookPath + 'show_version.yaml -vvv'
         retcode, output = subprocess.getstatusoutput(command)
-        self.assertEqual(retcode, 0)
-        self.assertIn('Current Runtime Version', output)
+        if retcode:
+            self.fail(output)
+        else:
+            self.assertNotEqual(output.find("Current Runtime Version"), -1)
 
     def test_fos_command_wait_for(self):
-        command = Cli + Inventory + PlaybookPath + 'wait_for.yaml -vvv | grep "Current Runtime Version"'
+        command = Cli + Inventory + PlaybookPath + 'wait_for.yaml -vvv'
         retcode, output = subprocess.getstatusoutput(command)
-        self.assertEqual(retcode, 0)
-        self.assertIn('Current Runtime Version', output)
+        if retcode:
+            self.fail(output)
+        else:
+            self.assertNotEqual(output.find("Current Runtime Version"), -1)
 
     def test_fos_command_wait_for_fails(self):
-        command = Cli + Inventory + PlaybookPath + 'wait_for_fail.yaml -vvv | grep "Current Runtime Version"'
+        command = Cli + Inventory + PlaybookPath + 'wait_for_fail.yaml -vvv'
         retcode, output = subprocess.getstatusoutput(command)
-        self.assertNotEqual(retcode, 0)
-        self.assertNotIn('Current Runtime Version', output)
+        if not retcode:
+            self.fail(output)
 
     def test_fos_command_match_any(self):
-        command = Cli + Inventory + PlaybookPath + 'match_any.yaml -vvv | grep "Current Runtime Version"'
+        command = Cli + Inventory + PlaybookPath + 'match_any.yaml -vvv'
         retcode, output = subprocess.getstatusoutput(command)
-        self.assertEqual(retcode, 0)
-        self.assertIn('Current Runtime Version', output)
+        if retcode:
+            self.fail(output)
+        else:
+            self.assertNotEqual(output.find("Current Runtime Version"), -1)
 
-    def test_fos_command_match_all(self):
-        command = Cli + Inventory + PlaybookPath + 'match_all.yaml -vvv | grep "Current Runtime Version"'
+    def test_fos_command_match_all_fail(self):
+        command = Cli + Inventory + PlaybookPath + 'match_all_fail.yaml -vvv'
         retcode, output = subprocess.getstatusoutput(command)
-        self.assertNotEqual(retcode, 0)
-        self.assertNotIn('Current Runtime Version', output)
+        if not retcode:
+            self.fail(output)
 
 
 if __name__ == '__main__':

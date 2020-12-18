14d28644a83f4172c826a2798181b10adcc1cdec
hs0210 <hus.fnst@cn.fujitsu.com>
2020-12-18 11:11:37 +0800

    add some fos_config local_unit_tests
    Signed-off-by: hs0210 <hus.fnst@cn.fujitsu.com>

--- tests/local_unit/test_fos_config.py
+++ tests/local_unit/test_fos_config.py
@@ -11,9 +11,37 @@ Inventory = '/home/cicd/inventory/inventory '
 PlaybookPath = 'tests/local_unit/playbooks/fos_config/'
 
 
+def find_updates(output):
+    begain = output.find('"updates"')
+    end = output.find(']', begain)
+    updates = output[begain: end + 1]
+    updates = updates.strip().split('\n')
+    return updates
+
+
 class Test(unittest.TestCase):
 
+    def test_fos_config_before(self):
+        command = Cli + Inventory + PlaybookPath + 'initial.yaml -vvv'
+        retcode, output = subprocess.getstatusoutput(command)
+        if retcode:
+            self.fail(output)
+        else:
+            updates = [
+                '"updates": [',
+                '        "clock timezone 9 minutes 0",',
+                '        "interface 0/13",',
+                '        "no lldp receive"',
+                '    ]'
+            ]
+            self.assertEqual(find_updates(output), updates)
+
     def test_fos_config_no_change(self):
+        command = Cli + Inventory + PlaybookPath + 'initial.yaml -vvv'
+        retcode, output = subprocess.getstatusoutput(command)
+        if retcode:
+            self.fail(output)
+
         command = Cli + Inventory + PlaybookPath + 'no_change.yaml -vvv'
         retcode, output = subprocess.getstatusoutput(command)
         if retcode:
@@ -62,6 +90,48 @@ class Test(unittest.TestCase):
             self.fail(output)
         else:
             self.assertNotEqual(output.find('changed=0'), -1)
+
+    def test_fos_config_match_exact(self):
+        command = Cli + Inventory + PlaybookPath + 'initial.yaml -vvv'
+        retcode, output = subprocess.getstatusoutput(command)
+        if retcode:
+            self.fail(output)
+
+        command = Cli + Inventory + PlaybookPath + 'match_exact.yaml -vvv'
+        retcode, output = subprocess.getstatusoutput(command)
+        if retcode:
+            self.fail(output)
+        else:
+            updates = [
+                '"updates": [',
+                '        "interface 0/13",',
+                '        "lldp transmit",',
+                '        "lldp receive",',
+                '        "lldp notification"',
+                '    ]'
+            ]
+            self.assertEqual(find_updates(output), updates)
+
+    def test_fos_config_replace_blocak(self):
+        command = Cli + Inventory + PlaybookPath + 'initial.yaml -vvv'
+        retcode, output = subprocess.getstatusoutput(command)
+        if retcode:
+            self.fail(output)
+
+        command = Cli + Inventory + PlaybookPath + 'replace_block.yaml -vvv'
+        retcode, output = subprocess.getstatusoutput(command)
+        if retcode:
+            self.fail(output)
+        else:
+            updates = [
+                '"updates": [',
+                '        "interface 0/13",',
+                '        "lldp transmit",',
+                '        "lldp receive",',
+                '        "lldp notification"',
+                '    ]'
+            ]
+            self.assertEqual(find_updates(output), updates)
 
 
 if __name__ == '__main__':
